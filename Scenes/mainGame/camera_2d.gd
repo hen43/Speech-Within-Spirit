34512d5f49d312bc8ca356f851c8c710ab7d599c
hen43 <whshle08@gmail.com>
2024-10-17 10:50:38 -0700

    jum
    glum

--- Scenes/mainGame/camera_2d.gd
+++ Scenes/mainGame/camera_2d.gd
@@ -1,6 +1,10 @@
 extends Camera2D
 
 @onready var player = get_node("../CharacterBody2D")
+@export var window_size = DisplayServer.window_get_size()
+
+func _ready() -> void:
+	print(window_size.length());
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -8,6 +12,6 @@ func _process(_delta: float) -> void:
 	position.y = lerp(position.y, player.position.y, 0.16)
 	
 	if Input.is_key_pressed(KEY_SPACE):
-		zoom += (Vector2(1,1) - zoom)/16;
+		zoom += (Vector2(2,2) - zoom)/16;
 	else:
-		zoom += (Vector2(1.2,1.2) - zoom)/16;
+		zoom += (Vector2(2.6,2.6) - zoom)/16;
--- Scenes/mainGame/label.gd
+++ Scenes/mainGame/label.gd
@@ -1,7 +1,7 @@
 extends Label
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	visible = false # Replace with function body.
+	visible = false; # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
